798bdf5135df3847b2c07d094b11bead2725db1c
Zhu Yijin <43716413+Zhuzhujin@users.noreply.github.com>
2019-06-20 09:39:02 +0800

    Add files via upload

---
+++ slide/js/js1.js
@@ -0,0 +1,24 @@
+// function byId(id){
+// 	return document.getElementById("main");
+// }
+var i = document.getElementById("main")
+console.log(i);
+// var index = 0,
+// 	timer=null,
+// 	pics=byId("banner").getElementsByTagName("div"),
+// 	len=pics.length;
+// function slideImg(){
+// 	var main = byId("main");
+// 	main.onmouseover=function(){
+
+// 	}
+// 	main.onmouseout=function(){
+// 		timer=setInterval(function(){
+// 			index++;
+// 			if(index>=len){
+// 				index=0;
+// 			}
+// 			console.log(index);
+// 		},2000);
+// 	}
+// }
